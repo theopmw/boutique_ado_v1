58a38f99c0f3222183e693df35711247b7ea757f
theopmw <theowrightdev@gmail.com>
2021-09-21 12:26:25 +0000

    Add productss views and templates

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     """ View to return index page """
-    return(render(request, 'home/index.html'))
+    return render(request, 'home/index.html')
